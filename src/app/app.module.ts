d2fddbf29b5a86cfe93ef134085feb69c3cea1f7
pazautumn <pazcontrerastello@gmail.com>
2018-04-04 09:10:32 -0300

    cambiando routing login/signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,6 @@ import { SearchComponent } from './search/search.component';
 import { MeetupComponent } from './meetup/meetup.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { GoogleCalendarComponent } from './google-calendar/google-calendar.component';
-import { GoogleMapsComponent } from './google-maps/google-maps.component';
 import { CommentsComponent } from './comments/comments.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -49,7 +48,6 @@ const routes: Routes = [
     MeetupComponent,
     PerfilComponent,
     GoogleCalendarComponent,
-    GoogleMapsComponent,
     CommentsComponent,
     WelcomeComponent
   ],
